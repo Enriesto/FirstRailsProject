e771fdb4e2046ac245713ab3ffe46aa852c3b3b1
Ernest Janzen <ejjthehun@hotmail.com>
2020-02-25 19:20:43 -0600

    Added a line to author controller to protect against n+1.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,8 +2,10 @@
 
 class AuthorsController < ApplicationController
   # GET /authors
+  # We're also taking care of any potential N+1 issues
+  # Future consideration: better way to put it?
   def index
-    @authors = Author.order(:name)
+    @authors = Author.includes(:books).order(:name)
   end
   # Automatically loads view:
   # views/authors/index.html.erb
